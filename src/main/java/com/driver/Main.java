6b8fe749942a204ea445a4043c25db6a528ad24b
Harshumhatre <95208474+Harshumhatre@users.noreply.github.com>
2023-02-15 17:44:01 +0530

    first commit module second program

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    RWOnly rwOnly = new RWOnly();
+    //rwOnly.name;
+    //Unresolved compilation problems
+	rwOnly.setName("Harshad");
+	System.out.println(rwOnly.getName());
 }
